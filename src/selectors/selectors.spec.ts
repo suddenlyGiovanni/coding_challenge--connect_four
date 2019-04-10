7727dbca53de3f632656ba49e5f0797b6104fd94
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2019-04-10 10:14:50 +0200

    feat(selectors.ts): added private fn getRightDiagonal

--- src/selectors/selectors.spec.ts
+++ src/selectors/selectors.spec.ts
@@ -1,6 +1,7 @@
-import { getColumn, getCell, getRow } from './selectors'
+import { getColumn, getCell, getRow, getRightDiagonal } from './selectors'
 import * as Fixture from '../fixture'
 import { Player } from '../index'
+import { Coordinates } from 'MyTypes'
 
 describe('getColumn', () => {
   it('happy path', () => {
@@ -24,5 +25,39 @@ describe('getCell', () => {
     expect(getTestBoardCell(0, 0)).toBe(Player.One)
     expect(getTestBoardCell(0, 1)).toBe(Player.Two)
     expect(getTestBoardCell(6, 1)).toBe(undefined)
+  })
+})
+
+describe('getRightDiagonal', () => {
+  const bottomLeft: Coordinates = [0, 0]
+  const bottomRight: Coordinates = [6, 0]
+  const topLeft: Coordinates = [0, 5]
+  const topRight: Coordinates = [6, 5]
+
+  test('happy path', () => {
+    expect(getRightDiagonal(topRight)).toEqual([
+      [1, 0],
+      [2, 1],
+      [3, 2],
+      [4, 3],
+      [5, 4],
+      [6, 5],
+    ])
+
+    expect(getRightDiagonal(bottomLeft)).toEqual([
+      [0, 0],
+      [1, 1],
+      [2, 2],
+      [3, 3],
+      [4, 4],
+      [5, 5],
+    ])
+  })
+  test('edge case: start from `topLeft`', () => {
+    expect(getRightDiagonal(topLeft)).toEqual([[0, 5]])
+  })
+
+  test('edge case: start from `bottomRight`', () => {
+    expect(getRightDiagonal(bottomRight)).toEqual([[6, 0]])
   })
 })
--- src/selectors/selectors.ts
+++ src/selectors/selectors.ts
@@ -1,4 +1,5 @@
-import { Board, Column, Row, Cell, X, Y } from 'MyTypes'
+import { Board, Column, Row, Cell, X, Y, Coordinates } from 'MyTypes'
+import { move } from '../move/move'
 
 export function getColumn(board: Board): (x: X) => Column {
   return x => board[x]
@@ -10,4 +11,21 @@ export function getRow(board: Board): (y: Y) => Row {
 
 export function getCell(board: Board): (x: X, y: Y) => Cell {
   return (x, y) => board[x][y]
+}
+
+export function getRightDiagonal(
+  coordinates: Coordinates
+): ReadonlyArray<Coordinates> {
+  // move diagonally down to the left to the starting point
+  const startingPoint: Coordinates = move(coordinates).diagonallyDownLeft() //?
+  const upRightIterable = move(startingPoint).upRight() //?
+
+  let rightDiagonalCoordinates = [startingPoint]
+  for (const iterator of upRightIterable) {
+    iterator //?
+    if (iterator !== undefined) {
+      rightDiagonalCoordinates.push(iterator) //?
+    }
+  }
+  return rightDiagonalCoordinates //?
 }
