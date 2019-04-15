99b3eedc43cc551b1ed5bf23b3458a154bdcadd8
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2019-04-15 15:53:04 +0200

    chore(index.d.ts): renamed Coordinates type to Point

--- src/selectors/selectors.ts
+++ src/selectors/selectors.ts
@@ -1,4 +1,4 @@
-import { Board, Column, Row, Cell, X, Y, Coordinates } from 'MyTypes'
+import { Board, Column, Row, Cell, X, Y, Point } from 'MyTypes'
 import { move } from '../move/move'
 
 export function getColumn(board: Board): (x: X) => Column {
@@ -13,29 +13,42 @@ export function getCell(board: Board): (x: X, y: Y) => Cell {
   return (x, y) => board[x][y]
 }
 
-export function getDiagonalRight(
-  coordinates: Coordinates
-): ReadonlyArray<Coordinates> {
+export function getDiagonalRight(point: Point): ReadonlyArray<Point> {
   // move diagonally down to the left to the starting point
-  const startingPoint: Coordinates = move(coordinates).diagonallyDownLeft() //?
+  const startingPoint: Point = move(point).diagonallyDownLeft() //?
   const upRightIterable = move(startingPoint).upRight() //?
 
-  const rightDiagonalCoordinates = [startingPoint]
-  for (const iterator of upRightIterable) {
-    iterator && rightDiagonalCoordinates.push(iterator) //?
-  }
+  const rightDiagonalCoordinates = [startingPoint, ...upRightIterable]
+
   return rightDiagonalCoordinates //?
 }
 
-export function getDiagonalLeft(
-  coordinates: Coordinates
-): ReadonlyArray<Coordinates> {
+export function getDiagonalLeft(point: Point): ReadonlyArray<Point> {
   // move diagonally down to the right to the starting point
-  const startingPoint: Coordinates = move(coordinates).diagonallyDownRight() //?
+  const startingPoint: Point = move(point).diagonallyDownRight() //?
   const upLeftIterable = move(startingPoint).upLeft() //?
-  const leftDiagonalCoordinates = [startingPoint]
-  for (const iterator of upLeftIterable) {
-    iterator && leftDiagonalCoordinates.push(iterator) //?
-  }
+  const leftDiagonalCoordinates = [startingPoint, ...upLeftIterable]
   return leftDiagonalCoordinates //?
 }
+
+// function getDiagonalCoordinates(
+//   board: Board
+// ): (x: X, y: Y, direction: 'upRight' | 'downRight') => any {
+//   return (x, y, direction) => {
+//     const _getCell = getCell(board)
+//     const _getCoordinates = getCoordinates(x, y)
+//     const nextDiagonalCoords = _getCoordinates[direction]()
+//     if (nextDiagonalCoords) {
+//       const [_x, _y] = nextDiagonalCoords
+//       const nextDiagonalCell = _getCell(_x, _y)
+//       if (nextDiagonalCell) {
+//       }
+//     }
+//   }
+// }
+
+// export function getDiagonal(
+//   board: Board
+// ): (x: X, y: Y) => (direction: 'upRight' | 'downRight') => any {
+//   return (x, y) => direction => {}
+// }
