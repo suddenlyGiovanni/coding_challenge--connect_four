31f3380865913d829a85685ce8758cfe78575062
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2019-04-17 15:12:30 +0200

    refactor(index.ts): refactored and moved updateColumn to update module

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,6 @@
-import { Player } from './index'
 import * as Fixture from './fixture'
 
-import { makeBoard, isColumnFull, updateColumn } from './index'
+import { makeBoard, isColumnFull } from './index'
 
 describe('makeBoard', () => {
   it('returns an empty board', () => {
@@ -20,17 +19,5 @@ describe('isColumnFull', () => {
 
   it('returns false if the column is already filled', () => {
     expect(isColumnFull(Fixture.fullColumn)).toBe(true)
-  })
-})
-
-describe('updateColumn', () => {
-  it('will throw if the column is already full', () => {
-    expect(() => updateColumn(Player.One, Fixture.fullColumn)).toThrow(
-      'This column is already full'
-    )
-  })
-
-  it('happy path', () => {
-    updateColumn(Player.One, Fixture.partiallyFilledColumn)
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -12,14 +12,3 @@ export function makeBoard(): Board {
 export function isColumnFull(column: Column): boolean {
   return column.length >= 6
 }
-
-export function updateColumn(player: Player, column: Column): Column {
-  if (isColumnFull(column)) {
-    throw new Error('This column is already full')
-  }
-  // [...] walk the array and add a player checker at the correct spot in the arr.
-  // 1 - reverse the array
-  const _column = column.slice()
-  _column.push(player)
-  return _column //?
-}
